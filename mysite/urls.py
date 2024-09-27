41bdb05cbdd68ec302c745e2b0f50b0ff820126c
maxgrove42 <131721751+maxgrove42@users.noreply.github.com>
2024-09-27 16:56:01 -0400

    change polls to root path

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from polls import views
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    path("", include("polls.urls")),
     path('admin/', admin.site.urls),
 ]
