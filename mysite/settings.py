a2800cfa26df92c690b36c53f4c0f4f61cf152ab
Max Grove <max.grove42@gmail.com>
2024-09-26 20:27:47 -0400

    Updates for Elastic Beanstalk

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jwm)_)#bgmv4gb5vid^)l(c=*fj8jsp=wg&^@x5g&3*1dt*txi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-polling-env.eba-2uhc5yw3.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['grove-polling-dev-dev.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
