b97c1215934ed491833a7f0dd22a76d0ba148a72
maxgrove42 <131721751+maxgrove42@users.noreply.github.com>
2024-10-11 16:54:26 -0400

    update .gitignore, add flake8, coverage, and black

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -9,7 +10,7 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-    
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
